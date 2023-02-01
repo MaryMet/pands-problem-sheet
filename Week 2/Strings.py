4ae8994bbc34616b6dedc9f77bb7681cc53e760b
Mary Metcalfe <G00425666@atu.ie>
2023-02-01 19:22:00 +0000

    week 2

---
+++ Week 2/Strings.py
@@ -0,0 +1,4 @@
+# strings.py
+# Author: Mary Metcalfe
+# messing with strings
+print ("Hello my name \t is Mary \nI am \t\t confuddled")
---
+++ Week 2/inputs.py
@@ -0,0 +1,8 @@
+# inputs.py
+# Author: Mary Metcalfe
+# messing with inputs
+name = input("enter your name:")
+print (f"your name is {name}")
+age = int(input("what is your age"))
+newage = age + 1
+print (f"next year you will be {newage}")
