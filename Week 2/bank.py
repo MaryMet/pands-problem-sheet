a9933e4d9792e683f7210849b667949c97a52df3
Mary Metcalfe <G00425666@atu.ie>
2023-02-02 19:32:33 +0000

    \pands-problem-sheet

---
+++ Week 2/bank.py
@@ -0,0 +1,13 @@
+# bank.py
+# author : Mary Metcalfe
+# messing with banks and money
+ 
+print ("we are going to add two numbers together")
+amount1 = input("Please enter first amount in cents")
+amount2 = input("Please enter second amount in cents")
+
+
+ans = float(amount1) + float(amount2)
+
+print ("The sum of amount1 and amount2 is: €" + str(ans))
+
