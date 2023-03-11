3bbfda2c3c786c6971e94e80f7b2fa86fdd2e37d
Mary Metcalfe <G00425666@atu.ie>
2023-03-11 17:18:23 +0000

    \

---
+++ count.py
@@ -0,0 +1,17 @@
+# Week 07
+# Program to count number of e's in a text file
+# My text file is called ecount.txt
+# Author : Mary Metcalfe
+
+def count_e ():
+    file = open ("ecount.txt")
+    content = file.read()
+    amounts = list("Ee")
+    count = 0
+
+    for amount in content:
+        if amount in amounts:
+            count+=1
+
+    return (count)
+print (f"Total number of es in file are: {count_e()}")
