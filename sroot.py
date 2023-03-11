8bf762883a4e978582986642d0c58fa6dae49352
Mary Metcalfe <G00425666@atu.ie>
2023-03-11 14:25:54 +0000

    \

--- sroot.py
+++ sroot.py
@@ -1,14 +1,14 @@
 # sroot
 
 
-def newton_sqrt(number, howmany):
+def newton_sqrt(number, howmany):  # function that uses the newtonian square root formula
     approx = 0.5*number
     for i in range(howmany):
         betterapprox = 0.5 * (approx + number/approx)
         approx = betterapprox
     return betterapprox
 
-x = float(input("Enter a number : "))
+x = float(input("Enter a number : ")) # prompt the user to enter any number that you want the square root of
 
-print (newton_sqrt (x, 10))
+print (newton_sqrt (x, 10)) # uses the newton_sqrt function ten times
 
