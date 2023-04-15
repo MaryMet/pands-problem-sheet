076d7e5202bf2b0a54c9b2f94daf08d18b4d5329
Mary Metcalfe <G00425666@atu.ie>
2023-04-15 17:01:43 +0100

    Putting the histogram in the legend

--- plottask.py
+++ plottask.py
@@ -8,8 +8,8 @@ mean = 5  # mean of the values is 5
 stdev = 2 # standard deviation is 2
 norm_data= mean + stdev *np.random.normal(size = 1000) # defining what we want in the histogram
 
-plt.hist(norm_data, color = "yellow", edgecolor = "black") # plot the normal data in a histogram - personalied to stand out
-plt.legend("norm_data")
+plt.hist(norm_data, color = "yellow", edgecolor = "black", label = "normdata") # plot the normal data in a histogram - personalied to stand out
+plt.legend() # putting the histogram into the legend - as per feedback
 
 xpoints = np.array(range(1,10)) # numbers to be cubed on the x axis
 ypoints = xpoints * xpoints * xpoints # cubes on the y axis
